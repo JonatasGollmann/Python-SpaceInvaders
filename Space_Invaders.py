704d4fd636129dfdd9a079930d96c0ee60036cd1
Jonatas Gollmann <86964636+JonatasGollmann@users.noreply.github.com>
2024-11-18 23:29:09 -0300

    V1.0.2 - Adicionado Inimigo com movimento.

--- Space_Invaders.py
+++ Space_Invaders.py
@@ -1,7 +1,8 @@
 import pygame
 from pygame.locals import *
 from sys import exit
-
+import random
+import time
 pygame.init()
 
 ##Configuração de tela
@@ -19,12 +20,21 @@ height = ship.get_height()
 xShip = largura/2 - width/2
 yShip = altura - height - 10
 velShip = 10
+yEnemy = random.randint(50, 150)
+xEnemy = random.randint(0, 720)
+velEnemy = 20
 
-bg = pygame.image.load('assets/bg_space.png')
-bg = pygame.transform.scale(bg, (largura, altura))
+
 def draw_bg():
+    bg = pygame.image.load('assets/bg_space.png')
+    bg = pygame.transform.scale(bg, (largura, altura))
     tela.blit(bg, (0, 0))
-    
+
+def showEnemy(x,y):
+    enemy = pygame.image.load('assets/enemy.png')
+    enemy = pygame.transform.scale(enemy, (25, 25))
+    tela.blit(enemy, (x, y))
+
 
 def showShip(x, y):
     tela.blit(ship, (x, y))
@@ -34,6 +44,8 @@ while run:
 
     clock.tick(60)
     draw_bg()
+    showShip(xShip, yShip)
+    showEnemy(xEnemy, yEnemy)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -41,17 +53,21 @@ while run:
 
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] and xShip > velShip:
+    if (keys[pygame.K_LEFT] or keys[pygame.K_a]) and xShip > velShip:
         xShip -= velShip
-    if keys[pygame.K_RIGHT] and xShip < 720 - width:
+    if (keys[pygame.K_RIGHT] or keys[pygame.K_d]) and xShip < 720 - width:
         xShip += velShip
-    if keys[K_UP] and yShip > velShip:
+    if (keys[pygame.K_UP] or keys[pygame.K_w]) and yShip > velShip:
         yShip -= velShip
-    if keys[K_DOWN] and yShip < 640 - height:
+    if (keys[K_DOWN] or keys[pygame.K_s]) and yShip < 640 - height:
         yShip += velShip
     
-    print(xShip, yShip)
-    showShip(xShip, yShip)
+    xEnemy += velEnemy
+    time.sleep(0.75)
+    if xEnemy >= largura - 25 or xEnemy <= 0:
+        velEnemy = -velEnemy
+        yEnemy += 25
+        
     pygame.display.update()
 
 pygame.quit()
